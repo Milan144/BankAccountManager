d6618d54d3129f7204311e8c81ea820267790bde
milan <milan.hommet@protonmail.com>
2022-11-23 16:52:47 +0100

    Chapitre gestion memoire

--- coursC2/main.c
+++ coursC2/main.c
@@ -64,6 +64,84 @@ int main(int argc, char *argv[]) {
         printf("Impossible d'ouvrir le fichier test.txt");
     }
 
+    // TODO :  MEMOIRE (Super important en C):
+
+    // Connaitre la taille allouee selon un type :
+    sizeof (int);
+
+    printf("char : %d octets\n", sizeof(char));
+    printf("int : %d octets\n", sizeof(int));
+    printf("long : %d octets\n", sizeof(long));
+    printf("double : %d octets\n", sizeof(double));
+
+    // Afficher taille d'un type custom marche aussi
+    sizeof(Coordonnees);
+
+    // malloc : demander la permission au systeme d'allouer de la memoire
+    // free : liberer la memoire
+
+    // 1 Appeler malloc
+    // 2 Verifier la valeur retournee
+    // 3 Liberer la memoire avec free
+
+
+    // ALLOCATION MANUELLE DE MEMOIRE :
+    //Prototype de malloc :
+    void* malloc(size_t nombreOctetsNecessaires);
+
+    int* memoireAllouee = NULL; //Creation pointeur sur int
+    memoireAllouee = malloc(sizeof(int)); // On demande au systeme d'allouer de la memoire pour un int
+
+    if(memoireAllouee == NULL)
+    {
+        exit(0); // On arrete le programme
+    }
+    // Sinon on peut continuer le programme
+
+    printf("Quel age avez vous? ");
+    scanf("Vous avez %d ans", *memoireAllouee);
+    // Liberer la memoire
+    //void free(void* pointeur);
+    free(memoireAllouee);
+
+
+    // Tableau dynamique :
+    int nombreDAmis = 0, i = 0;
+    int* ageAmis = NULL;
+
+    printf("Combien avez-vous d'amis ? ");
+    scanf("%d", &nombreDAmis);
+
+    if(nombreDAmis > 0)
+    {
+        ageAmis = malloc(nombreDAmis * sizeof(int));
+
+        if (ageAmis == NULL)
+        {
+            exit(0);
+        }
+
+        for (i = 0; i < nombreDAmis; i++)
+        {
+            printf("Quel age a votre ami numero %d ? ", i + 1);
+            scanf("%d", &ageAmis[i]);
+        }
+
+        printf("\n\nVos amis ont les ages suivants :\n");
+        for (i = 0 ; i < nombreDAmis ; i++)
+        {
+            printf("%d ans\n", ageAmis[i]);
+        }
+
+        free(ageAmis);
+    }
+
+    // TODO : Recuperation securisee d'une chaine de caracteres
+    // gets : lit toute une chaine de caracteres
+    // fgets : lit une chaine de caracteres jusqu'a un caractere donne
+
+    // Prototype de fgets : char *fgets(char *str, int num, FILE *stream);
+
     return 0;
 }
 
