cff06359c972184799728b1f9187e66512c5a580
milan <milan.hommet@protonmail.com>
2022-11-25 15:10:58 +0100

    start of the application

--- BankManagement/main.c
+++ BankManagement/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, welcome on the bank app!\n Press 1 to create an account\n Press 2 to login\n Press 3 to exit\n");
+    int choice;
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            printf("You chose to create an account\n");
+            break;
+        case 2:
+            printf("You chose to login\n");
+            char username[20];
+            char password[20];
+            printf("Enter your username: ");
+            scanf("%s", username);
+            printf("Enter your password: ");
+            scanf("%s", password);
+
+
+        case 3:
+            printf("You chose to exit\n");
+            break;
+        default:
+            printf("You chose an invalid option\n");
+            break;
+    }
+
     return 0;
 }
+
+
---
+++ BankManagement/main.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int login(char username, char password);
+
+int registration(char username, char password);
+{
+          
+}
